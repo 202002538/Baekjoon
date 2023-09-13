c8645e1236cb5d0df3770e8a99cdf37f08a2d683
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2023-09-13 19:06:06 +0900

    [Gold IV] Title: 사이클 게임, Time: 1700 ms, Memory: 50684 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+if __name__ == '__main__':
+    def find(parent, x):
+        if x != parent[x]:
+            parent[x] = find(parent, parent[x])
+        return parent[x]
+
+    def union(parent, a, b):
+        a = find(parent, a)
+        b = find(parent, b)
+        if a > b:
+            parent[a] = b
+        else:
+            parent[b] = a
+
+    n, m = map(int, input().split())
+
+    parent = [i for i in range(n+1)]
+    cycle = 0
+    for j in range(1, m+1):
+        a, b = map(int, input().split())
+        if find(parent, a) != find(parent, b):
+            union(parent, a, b)
+        else:
+            if not cycle:
+                cycle = j
+    print(cycle)
