fff5ad7dced1d37f7f39d4acf408b107a260c503
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2023-10-10 15:55:47 +0900

    [Gold V] Title: 복권 + 은행, Time: 100 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Gold/13258. 복권 ＋ 은행/복권 ＋ 은행.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+if __name__ == '__main__':
+    n = int(input())
+    money = list(map(int, input().split()))
+    price = int(input()) #당첨금액
+    week = int(input()) #진행 주수
+
+    #초기테이블 세팅
+    dp = [[0] * (n + 1) for _ in range(week + 1)]
+    dp[0][1:1+n] = money
+
+    #dp
+    for i in range(week):
+        for j in range(n+1):
+            #다음주 내 잔고에 추가될 기대값 = (1/ 이번주 모든 사람의 잔고합) * 내 잔고 * 당첨금
+            expectation = 1/sum(dp[i]) * dp[i][j] * price
+            dp[i+1][j] = dp[i][j] + expectation
+
+    print(dp[week][1]) #1번사람(강호)의 기댓값 출력
