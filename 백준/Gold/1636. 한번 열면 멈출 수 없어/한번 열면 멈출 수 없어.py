c60503e166228283fe63d2550b847ed6f1ca8c11
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2023-10-28 17:59:35 +0900

    [Gold IV] Title: 한번 열면 멈출 수 없어, Time: 1544 ms, Memory: 40532 KB -BaekjoonHub

--- 백준/Gold/1636. 한번 열면 멈출 수 없어/한번 열면 멈출 수 없어.py
+++ 백준/Gold/1636. 한번 열면 멈출 수 없어/한번 열면 멈출 수 없어.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
         s, e = map(int, input().split())
         pringles.append((s, e))
 
-
     def greedy(stress):
         stress_list = [stress] #스트레스 변화 과정
         reduce = 0 #줄어든 수명
@@ -26,11 +25,12 @@ if __name__ == '__main__':
         return reduce, stress_list
 
     result, tmp = INF, []
-    for g in range(pringles[0][0], pringles[0][1]+1):
+    for g in range(pringles[0][0], pringles[0][1]+1): #스트레스 시작점 -> 첫번째 통으로 가능한 모든 범위
         r, l = greedy(g)
-        if result > r:
+        if result > r: #줄어든 수명 최대한 작은 경우 저장
             result = r
             tmp = l
+            
     print(result)
     for t in tmp:
         print(t)
