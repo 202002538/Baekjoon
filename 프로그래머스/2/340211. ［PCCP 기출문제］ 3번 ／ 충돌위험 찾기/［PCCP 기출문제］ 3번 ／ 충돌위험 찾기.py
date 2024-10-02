73ee4f9a4f41dcd82e39306e46c5b00834e29625
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-10-02 10:13:55 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 562.53 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -1,22 +1,17 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 from collections import Counter
 
 def solution(points, routes):
     path = []
-    accident = set()
-
-    for i in range(len(routes)):
-        x, y = points[routes[i][0]-1]
-        path.append((x, y, 0))
 
     for i in range(len(routes)):
         time = 0
+        x, y = points[routes[i][0]-1]
+        path.append((x, y, 0))
         for j in range(len(routes[i])-1):
             x1, y1 = points[routes[i][j]-1]
             x2, y2 = points[routes[i][j+1]-1]
-
             while x1 != x2 or y1 != y2:
                 time += 1
                 if x1 < x2:
@@ -27,7 +22,6 @@ def solution(points, routes):
                     y1 += 1
                 elif y1 > y2:
                     y1 -= 1
-
                 path.append((x1, y1, time))
     
     answer = 0
