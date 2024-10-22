4841b1021db3ce9492c5300fd2063c90dddcdc3b
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-10-22 18:56:36 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 1.94 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.py
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.py
@@ -1,6 +1,5 @@
 import sys
 input = sys.stdin.readline
-import copy
 
 def _10toN(n, q):
     rev_base = ''
@@ -16,39 +15,36 @@ def solution(expressions):
     for e in expressions:
         exp = e.split(" ")
         a, op, b, c = exp[0], exp[1], exp[2], exp[4]
+        #수식 내 가장 큰 수로 진법 범위 조정
         nums = list(filter(lambda x: x > (int(max(a+b+c)) if c != 'X' else int(max(a+b))), nums))
-        if c == 'X': 
+        if c == 'X': #결과 모르는 식은 따로 저장해둠
             mystery.append(e)
             continue
+        #남은 진법으로 수식 계산 -> 맞지 않는 진법 삭제
         tmp_nums = nums.copy()
         for n in nums:
-            a1, b1, c1 = int(str(a), n), int(str(b), n), int(str(c), n)
-            if op == '+':
-                tmp = a1 + b1
-            else:
-                tmp = a1 - b1
-            if tmp != c1:
+            tmp = eval(str(int(str(a), n)) + op + str(int(str(b), n)))
+            if tmp != int(str(c), n):
                 tmp_nums.remove(n)
         nums = tmp_nums
 
+    #가능한 진법들과 결과 모르는 식 비교
+    #결과값이 유일한지 아닌지 확인, 채워넣기
     for i in range(len(mystery)):
         mys = mystery[i].split(" ")
         a, op, b = mys[0], mys[1], mys[2]
         able_result = []
         for n in nums:
-            a1, b1 = int(str(a), n), int(str(b), n)
-            if op == '+':
-                tmp = a1 + b1
-            else:
-                tmp = a1 - b1
+            tmp = eval(str(int(str(a), n)) + op + str(int(str(b), n)))
             tmp = _10toN(tmp, n)
             if tmp not in able_result:
                 able_result.append(tmp)
-        if len(able_result) == 1:
+
+        if len(able_result) == 1: #유일함 - 채워넣기
             if able_result[0] == '':
                 able_result[0] = '0'
             mystery[i] = mystery[i].replace('X', str(able_result[0]))
-        else:
+        else: #유일하지 않음 - ?로 채우기
             mystery[i] = mystery[i].replace('X', '?')
 
     return mystery
