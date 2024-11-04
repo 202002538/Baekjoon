3139477b05e4a4eff61d0a41fac7bdca2351c2e3
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-04 17:30:50 +0900

    [Gold IV] Title: 플로이드, Time: 340 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,30 +1,28 @@
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+INF = 1e9
 
 if __name__ == '__main__':
     n = int(input())
     m = int(input())
+    floyd = [[INF] * (n + 1) for _ in range(n + 1)]
 
-    #최단거리 저장 리스트 세팅
-    graph = [[INF] * (n+1) for _ in range(n+1)]
-    for a in range(n+1):
-        graph[a][a] = 0
+    for i in range(n+1):
+        floyd[i][i] = 0
     for _ in range(m):
         a, b, c = map(int, input().split())
-        graph[a][b] = min(graph[a][b], c) #노선이 여러개일 수 있음
+        floyd[a][b] = min(floyd[a][b], c)
 
-    #플로이드 워셜
-    for k in range(1, n+1):
-        for a in range(1, n+1):
-            for b in range(1, n+1):
-                graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+    for k in range(1, n+1): 
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if floyd[i][k] + floyd[k][j] < floyd[i][j]:
+                    floyd[i][j] = floyd[i][k] + floyd[k][j]
 
-    #출력
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            if graph[a][b] == INF:  # 도달할 수 없는 경우
-                print("0", end=" ")
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if floyd[i][j] == INF:
+                print(0, end=" ")
             else:
-                print(graph[a][b], end=" ")
+                print(floyd[i][j], end=" ")
         print()
