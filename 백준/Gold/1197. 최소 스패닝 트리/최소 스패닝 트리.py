e1d1f8530ecc5652a172e4663f236d3baa69b708
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-04 20:06:31 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 264 ms, Memory: 48436 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -3,35 +3,34 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 
 if __name__ == '__main__':
-    def find(parent, x):
-        if x != parent[x]:
-            parent[x] = find(parent, parent[x])
+    def find(x):
+        if parent[x] != x:
+            parent[x] = find(parent[x])
         return parent[x]
 
-    def union(parent, a, b):
-        a = find(parent, a)
-        b = find(parent, b)
-        if a > b:
-            parent[a] = b
+    def union(x, y):
+        x = find(x)
+        y = find(y)
+        if x < y:
+            parent[y] = x
         else:
-            parent[b] = a
+            parent[x] = y
 
-    #input
     v, e = map(int, input().split())
-    edges = []
+    parent = [i for i in range(v+1)]
+    graph = []
     for _ in range(e):
         a, b, c = map(int, input().split())
-        edges.append((c, a, b))
+        graph.append((c, a, b))
+    graph = sorted(graph)
 
-    edges.sort()
-
-    #크루스칼
-    result = 0
-    parent = [i for i in range(v+1)]
-    for edge in edges:
-        c, a, b = edge
-        if find(parent, a) != find(parent, b):
-            union(parent, a, b)
-            result += c
-
-    print(result)
+    total = 0
+    need = v-1
+    for c, a, b in graph:
+        if need == 0:
+            break
+        if find(a) != find(b):
+            total += c
+            union(a, b)
+            need -= 1
+    print(total)
