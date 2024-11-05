35aec045141f73cbf385346449efe3db8fb8a70f
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-05 14:28:29 +0900

    [Gold IV] Title: 해킹, Time: 736 ms, Memory: 48028 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -1,46 +1,38 @@
-import heapq
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+INF = 1e9
+import heapq
 
 if __name__ == '__main__':
     result = []
-    case = int(input())
-    for _ in range(case):
+    for _ in range(int(input())):
         n, d, c = map(int, input().split())
         graph = [[] for _ in range(n+1)]
         for _ in range(d):
             a, b, s = map(int, input().split())
             graph[b].append((a, s))
 
-        #최소 시간 저장
-        distance = [INF] * (n+1)
+        reach = [INF] * (n+1)
 
-        #시작점 세팅
-        distance[c] = 0
-        hq = []
-        heapq.heappush(hq, (0, c))
-
-        #다익스트라
-        while hq:
-            dis, now = heapq.heappop(hq)
-            if dis > distance[now]:
+        q = []
+        heapq.heappush(q, (0, c)) 
+        reach[c] = 0
+        while q:
+            dis, now = heapq.heappop(q)
+            if dis > reach[now]:
                 continue
-            for i in graph[now]:
-                temp = dis + i[1]
-                if temp < distance[i[0]]:
-                    distance[i[0]] = temp
-                    heapq.heappush(hq, (temp, i[0]))
+            for sub, time in graph[now]:
+                if dis + time < reach[sub]:
+                    reach[sub] = dis + time
+                    heapq.heappush(q, (reach[sub], sub))
 
-        #감염된 수와 마지막 감염시간 저장
-        count = 0
-        lastest = 0
-        for j in distance:
-            if j != INF:
-                count += 1
-                lastest = max(lastest, j)
-        result.append([count, lastest])
+        infected, time = 0, 0
+        for r in reach:
+            if r == INF:
+                continue
+            infected += 1
+            time = max(time, r)
+        result.append([infected, time])
 
-    #최종결과 프린트
     for k in result:
         print(*k)
