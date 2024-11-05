37a7254a72157d3700a62ac8514b09b7360e4e39
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-05 17:55:48 +0900

    [Gold II] Title: 문제집, Time: 180 ms, Memory: 40632 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -1,45 +1,28 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
-from heapq import heappush, heappop
+import heapq
 
 if __name__ == '__main__':
-    def find(parent, x):
-        if x != parent[x]:
-            parent[x] = find(parent, parent[x])
-        return parent[x]
-
-    def union(parent, a, b):
-        a = find(parent, a)
-        b = find(parent, b)
-        if a > b:
-            parent[a] = b
-        else:
-            parent[b] = a
-
-    #input
     n, m = map(int, input().split())
-    graph = [[] for _ in range(n+1)]
+    prob = [[] for _ in range(n+1)]
     indegree = [0] * (n+1)
-
     for _ in range(m):
         a, b = map(int, input().split())
-        graph[a].append(b)
         indegree[b] += 1
+        prob[a].append(b)
 
-    #위상 정렬 시작
-    result = []
-    hq = [] #힙 이용, 선택지 중 '가능하면 쉬운 문제'부터 풀도록
+    hq = []
     for i in range(1, n+1):
         if indegree[i] == 0:
-            heappush(hq, i)
+            heapq.heappush(hq, i)
 
+    result = []
     while hq:
-        now = heappop(hq)
+        now = heapq.heappop(hq)
         result.append(now)
+        for p in prob[now]:
+            indegree[p] -= 1
+            if indegree[p] == 0:
+                heapq.heappush(hq, p)
 
-        for j in graph[now]:
-            indegree[j] -= 1
-            if indegree[j] == 0:
-                heappush(hq, j)
     print(*result)
