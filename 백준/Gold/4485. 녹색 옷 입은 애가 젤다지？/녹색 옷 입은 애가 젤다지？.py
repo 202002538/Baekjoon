a75ed12b468e88d2d82b5e7cf9c752ee049f1f26
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-06 16:01:22 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 144 ms, Memory: 33216 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,38 +1,38 @@
-import heapq
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+INF = 1e9
+import heapq
 
 if __name__ == '__main__':
-    n = int(input())
-    count = 1
-    while n:
-        graph = []
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    result = []
+    while True:
+        n = int(input())
+        if not n:
+            break
+
+        cave = []
+        loss = [[INF] * n for _ in range(n)]
         for _ in range(n):
-            graph.append(list(map(int, input().split())))
-        distance = [[INF] * n for _ in range(n)]
+            cave.append(list(map(int, input().split())))
 
-        dx = [-1, 0, 1, 0]
-        dy = [0, -1, 0, 1]
-
-        #시작점 세팅
-        distance[0][0] = graph[0][0]
+        loss[0][0] = cave[0][0]
         hq = []
-        heapq.heappush(hq, (graph[0][0], 0, 0)) #현재 개수, 좌표 순서
-
+        heapq.heappush(hq, (cave[0][0], 0, 0))
         while hq:
             dis, x, y = heapq.heappop(hq)
-            if dis > distance[x][y]:
+            if dis > loss[x][y]:
                 continue
             for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < n:
-                    cost = dis + graph[nx][ny]
-                    if cost < distance[nx][ny]:
-                        distance[nx][ny] = cost
-                        heapq.heappush(hq, (cost, nx, ny))
-        #출력
-        print("Problem %d: %d" %(count, distance[n-1][n-1]))
-        count += 1
-        n = int(input())
+                nx, ny = x + dx[i], y + dy[i]
+                if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                    continue
+                cost = loss[x][y] + cave[nx][ny]
+                if loss[nx][ny] > cost:
+                    loss[nx][ny] = cost
+                    heapq.heappush(hq, (cost, nx, ny))
+        result.append(loss[n-1][n-1])
+
+    for i, r in enumerate(result):
+        print(f'Problem {i+1}: {r}')
