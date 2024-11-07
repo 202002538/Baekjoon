7859ddae5ebf8b964144ed84c118dbbb55f07564
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-07 14:06:51 +0900

    [Gold III] Title: 파티, Time: 48 ms, Memory: 34236 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,46 +1,40 @@
-import heapq
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+INF = 1e9
+import heapq
 
 if __name__ == '__main__':
     n, m, x = map(int, input().split())
-
-    # 그래프 입력받기
-    original_graph = [[] for _ in range(n + 1)]
-    # 역방향으로도 저장: 모든 마을 -> 파티장소로 가는길 한번에 구함
-    reverse_graph = [[] for _ in range(n + 1)]
+    original = [[] for _ in range(n+1)]
+    reverse = [[] for _ in range(n+1)]
 
     for _ in range(m):
-        a, b, c = map(int, input().split())
-        original_graph[a].append((b, c))
-        reverse_graph[b].append((a, c))
+        a, b, t = map(int, input().split())
+        original[a].append((b, t))
+        reverse[b].append((a,t))
 
-    def dijkstra(start, graph):
-        q = []
-        distance = [INF] * (n + 1)
-
-        # 시작노드에 대한 초기화
-        heapq.heappush(q, (0, start))
-        distance[start] = 0
-
-        while q:
-            dist, now = heapq.heappop(q)
-            if distance[now] < dist:
+    def dijkstra(graph, x):
+        distance = [INF] * (n+1)
+        distance[x] = 0
+        hq = []
+        heapq.heappush(hq, (0, x))
+        while hq:
+            dis, now = heapq.heappop(hq)
+            if dis > distance[now]:
                 continue
-            for i in graph[now]:
-                cost = dist + i[1]
-                if cost < distance[i[0]]:
-                    distance[i[0]] = cost
-                    heapq.heappush(q, (cost, i[0]))
+            for g, time in graph[now]:
+                cost = dis + time
+                if cost < distance[g]:
+                    distance[g] = cost
+                    heapq.heappush(hq, (cost, g))
         return distance
 
-    #각 학생 x로 오는 시간
-    distance = dijkstra(x, reverse_graph)
+    # 각자 마을 -> 파티장으로의 다익스트라(파티장에서의 역순으로)
+    go = dijkstra(reverse, x)
+    # 파티장 -> 각자 마을로의 다익스트라
+    back = dijkstra(original, x)
 
-    #각 학생 가는 시간
-    distance2 = dijkstra(x, original_graph)
+    result = 0
     for i in range(1, n+1):
-        distance[i] += distance2[i]
-
-    print(max(distance[1:]))
+        result = max(result, go[i]+back[i])
+    print(result)
