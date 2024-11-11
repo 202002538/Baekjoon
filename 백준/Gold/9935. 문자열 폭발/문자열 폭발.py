76275cae02d97048d0081ad062a3c7b8e02c67a3
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-11 17:29:22 +0900

    [Gold IV] Title: 문자열 폭발, Time: 344 ms, Memory: 42300 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    strr = input()[:-1]
+    bomb = list(input()[:-1])
+    n = len(bomb)
+
+    stack = []
+    for s in strr:
+        stack.append(s)
+        if stack[-n:] == bomb:
+            for _ in range(n):
+                stack.pop()
+
+    print(''.join(stack)) if stack else print("FRULA")
