994f18bca28698ae355f2db7dcf0b97dd36b694e
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-15 13:48:47 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 288 ms, Memory: 55220 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+result = 0
+left, right = 0, 1
+dic = defaultdict(int)
+dic[nums[left]] += 1
+
+while right < n:
+    dic[nums[right]] += 1
+    if dic[nums[right]] > k:
+        result = max(result, right-left)
+        while True:
+            dic[nums[left]] -= 1
+            if nums[left] == nums[right]:
+                left += 1
+                break
+            left += 1
+    result = max(result, right-left+1)
+    right += 1
+
+print(result)
