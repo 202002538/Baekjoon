29dfecb054d3ac575eeb3206614af671f46de9c9
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2024-11-20 13:15:18 +0900

    [Gold II] Title: 공항, Time: 136 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -1,34 +1,29 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
 
-if __name__ == '__main__':
-    def find(parent, x):
-        if x != parent[x]:
-            parent[x] = find(parent, parent[x])
-        return parent[x]
+g = int(input())
+p = int(input())
 
-    def union(parent, a, b):
-        a = find(parent, a)
-        b = find(parent, b)
-        if a > b:
-            parent[a] = b
-        else:
-            parent[b] = a
+def find(a):
+    if parent[a] != a:
+        parent[a] = find(parent[a])
+    return parent[a]
 
-    #input
-    g = int(input())
-    p = int(input())
-    parent = [i for i in range(g+1)]
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
-    #번호 확인
-    result = 0
-    for i in range(p):
-        num = int(input())
-        root = find(parent, num) #루트 노드는 "현재 도킹가능한 가장 큰 번호"
-        if root == 0: #도킹 가능한 자리x
-            break
-        union(parent, root, root-1) #도킹완료. 루트노드를 1 줄인다.
-        result += 1
-
-    print(result)
+result = 0
+parent = [i for i in range(g+1)]
+for _ in range(p):
+    num = int(input())
+    gate = find(num)
+    if gate == 0:
+        break
+    union(gate, gate-1)
+    result += 1
+print(result)
