84260539d994bbbdf1f71b7047afe278bdef68b6
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2025-06-11 13:49:12 +0900

    [Silver V] Title: 전철 통학, Time: 360 ms, Memory: 115624 KB -BaekjoonHub

--- 백준/Silver/33991. 전철 통학/전철 통학.py
+++ 백준/Silver/33991. 전철 통학/전철 통학.py
@@ -1,15 +1,18 @@
-st = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
+
+x1, y1, x2, y2, x3, y3 = map(int, input().split())
 q = int(input())
 
 for _ in range(q):
-    now = list(map(int, input().split()))
-    x, y = now[0], now[1]
-    now = now[2:]
-    result = []
+    x, y, t1, t2, t3 = list(map(int, input().split()))
 
-    for i in range(3):
-        m = abs(x - st[2 * i]) + abs(y - st[2 * i + 1])
-        tmp = ((m + now[i] - 1) // now[i]) * now[i]
-        result.append(tmp)
+    m1 = abs(x - x1) + abs(y - y1)
+    m2 = abs(x - x2) + abs(y - y2)
+    m3 = abs(x - x3) + abs(y - y3)
 
-    print(min(result))
+    time1 = ((m1 + t1 - 1) // t1) * t1
+    time2 = ((m2 + t2 - 1) // t2) * t2
+    time3 = ((m3 + t3 - 1) // t3) * t3
+
+    print(min(time1, time2, time3))
