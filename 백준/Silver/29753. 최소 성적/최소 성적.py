2c8e562fe43cf550406ccbdcaf7e07060e49e164
chaeyeonLee <90441461+202002538@users.noreply.github.com>
2025-06-11 16:19:40 +0900

    [Silver III] Title: 최소 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/29753. 최소 성적/최소 성적.py
+++ 백준/Silver/29753. 최소 성적/최소 성적.py
@@ -12,27 +12,24 @@ llist = ['F', 'D0', 'D+', 'C0', 'C+', 'B0', 'B+', 'A0', 'A+']
 # n, x 읽어서 x도 정수(×100)로 변환
 n_str, x_str = input().split()
 n = int(float(n_str))
-if '.' in x_str:
-    w, f = x_str.split('.')
-    x = int(w) * 100 + int((f + '00')[:2])
-else:
-    x = int(x_str) * 100
+w, f = x_str.split('.')
+x = int(w) * 100 + int(f)
 
-grade = 0
-score = 0
+#현재까지의 평균평점 계산
+grade, score = 0, 0
 for _ in range(n - 1):
     g, s = input().split()
     grade += int(g)
     score += level[s] * int(g)
+
 last = int(input())
-grade += last
 
 # 남은 한 과목 등급별로 확인
 for g in llist:
     total = score + level[g] * last
-    # (평균 × 1000)을 내림
-    tmp = total * 10 // grade
-    # (평균 × 100)을 내림
+    # 셋째 자리에서 버림
+    tmp = total * 10 // (grade + last)
+    # 둘째 자리에서 버림
     final = tmp // 10
     if final > x:
         print(g)
